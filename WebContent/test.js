f0cf0a81cd30135532366e236b5ec6caa84ac1fa
Weiming <wingalong@gmail.com>
2017-04-17 10:36:37 +0800

    Test ES6 and Vue

---
+++ WebContent/test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by zhuang_w-pc on 4/17/2017.
+ */
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment: state => state.count++,
+        decrement: state => state.count--
+    }
+});
+
+const app = new Vue({
+    el: '#app',
+    computed: {
+        count () {
+            return store.state.count
+        }
+    },
+    methods: {
+        increment () {
+            store.commit('increment')
+        },
+        decrement () {
+            store.commit('decrement')
+        }
+    }
+});
