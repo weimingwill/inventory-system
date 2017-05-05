ee1f3be37f0eff5cf4a761b7538b8e3f3507a366
Weiming <wingalong@gmail.com>
2017-05-05 19:05:07 +0800

    Implement purchase function with bug in variants

---
+++ WebContent/src/utils/setting.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zhuang_w-pc on 5/2/2017.
+ */
+export const PRODUCTS = 'products';
+export const VARIANTS = 'variants';
+export const PURCHASE_ORDERS = 'purchaseOrders';
+
+export const MODULE_WAREHOUSE = 'warehouse';
+export const OBJ_WARHEOUSE = 'warehouses';
